12df38d8d39175d74228c19a70576bc02aeb22c0
Amilcar Mula <amilcarmhula.am@gmail.com>
2019-06-30 15:48:28 +0200

    HTML basico e PHP
    Estrutura base de pagina web

---
+++ WEB/01-fundamentos-basicos.php
@@ -0,0 +1,18 @@
+<html>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>fFundamentos basicos</title>
+</head>
+<body>
+	<?php
+	// comentarios em PHP!
+	/*
+	outros comentarios!
+	*/
+		echo "Meu nome é Amilcar!<br>"; 
+		print("Amilcar");
+	?>
+	
+</body>
+</html>
